456cd86bfc0b26f3c83ec4d1a20587b8614f6305
coderepositor <gopika8747@gmail.com>
2024-06-17 23:17:26 +0530

    advanced calculator

---
+++ advcalculator.py
@@ -0,0 +1,42 @@
+import math
+def addition():
+
+    a=int(input())
+    b=int(input())
+    print('sum',a+b)
+def subtraction():
+    n1=int(input())
+    n2=int(input())
+    print('difference',a-b)
+def product():
+    a=int(input())
+    b=int(input())
+    print('product',a*b)
+def division():
+    d1=int(input())
+    d2=int(input())
+    if(d2==0):
+        print("cannot divide")
+    print('division',d1/d2)
+def square():
+    a=int(input())
+    print(a**2)
+def squareroot():
+    a=int(input())
+    print("squareroot",math.sqrt(a))
+
+print("enter choice")
+choice=input()
+print("enter  number")
+if (choice=='add'):
+    addition()
+elif(choice=='subtract'):
+    subtraction()
+elif(choice=='multiply'):
+    product()
+elif(choice=='divide'):
+    division()
+elif(choice=="square"):
+    square()
+elif(choice=="squareroot"):
+    squareroot()
